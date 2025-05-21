2269be14be6c6217113505d1ea73406497fed52c
ArtemArs211 <arsenyev21@mail.ru>
2025-05-21 23:33:41 +0300

    Initial commit

---
+++ src/main/java/org/example/Boot_MVC/BootMvcApplication.java
@@ -0,0 +1,13 @@
+package org.example.Boot_MVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMvcApplication.class, args);
+	}
+
+}
